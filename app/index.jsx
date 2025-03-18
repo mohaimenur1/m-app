239d4bc2fdbfc258ced1d0ec1fe5cfb298bc3d8b
Mohi Minur <68849665+mohaimenur1@users.noreply.github.com>
2025-03-18 15:59:03 +0600

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,88 @@
+import { Button, Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Logo from "@/assets/images/logo.png";
+import splashImage from "@/assets/images/Onboarding-img.png";
+import { Link } from "expo-router";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.contentContainer}>
+        {/* logo image */}
+        <Image source={Logo} style={styles.logoImage} />
+        <Image source={splashImage} style={styles.splashImage} />
+        <Text style={styles.text}>
+          Discover Endless Possibilities with{" "}
+          <Text style={styles.highlightedText}>Aora</Text>
+        </Text>
+        <Text style={styles.captionText}>
+          Where Creativity Meets Innovation: Embark on a Journey of Limitless
+          Exploration with Aora
+        </Text>
+        <Pressable
+          style={styles.button}
+          //   onPress={() => console.log("Button pressed")}
+        >
+          <Link href="/sign-up">
+            <Text style={styles.buttonText}>Continue with Email</Text>
+          </Link>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#161622",
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logoImage: {
+    width: 150,
+    // height: 150,
+    resizeMode: "contain",
+  },
+  splashImage: {
+    width: 375,
+    height: 298,
+    resizeMode: "contain",
+  },
+  text: {
+    color: "#FFFFFF",
+    fontSize: 30,
+    fontWeight: "semibold",
+    textAlign: "center",
+  },
+  highlightedText: {
+    color: "#FF8E01",
+    fontWeight: "semibold",
+    fontSize: 30,
+    marginBottom: 20,
+  },
+  captionText: {
+    color: "#CDCDE0",
+    fontSize: 14,
+    textAlign: "center",
+  },
+  button: {
+    width: 327,
+    height: 58,
+    backgroundColor: "#FF8E01",
+    borderRadius: 8,
+    padding: 10,
+    marginTop: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "black",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
---
+++ app/sign-up.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+import {
+  Image,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import Logo from "@/assets/images/logo.png";
+import { Link } from "expo-router";
+
+const signUp = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.contentContainer}>
+        <Image source={Logo} style={styles.logoImage} />
+        <Text style={styles.title}>Sign Up</Text>
+        {/* username */}
+        <View style={styles.usernameContainer}>
+          <Text style={styles.usernameTitle}>Username</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your username"
+            placeholderTextColor="#7B7B8B"
+          />
+        </View>
+        {/* eamil */}
+        <View style={styles.emailContainer}>
+          <Text style={styles.usernameTitle}>Email</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your email"
+            placeholderTextColor="#7B7B8B"
+          />
+        </View>
+        {/* password */}
+        <View style={styles.passwordContainer}>
+          <Text style={styles.usernameTitle}>Password</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your Password"
+            placeholderTextColor="#7B7B8B"
+          />
+        </View>
+        {/* button  */}
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </Pressable>
+        <Text style={styles.redirectLink}>
+          Already have an account?{" "}
+          <Link href="/sign-in">
+            <Text style={styles.login}>Login</Text>
+          </Link>
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default signUp;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#161622",
+  },
+  contentContainer: {
+    marginHorizontal: 24,
+    marginVertical: 40,
+    marginTop: 100,
+  },
+  logoImage: {
+    width: 150,
+    height: 56,
+    resizeMode: "contain",
+    marginBottom: 35,
+  },
+  title: {
+    color: "#FFFFFF",
+    fontSize: 30,
+    fontWeight: "semibold",
+    textAlign: "left",
+    marginBottom: 20,
+  },
+  usernameContainer: {
+    marginBottom: 20,
+  },
+  emailContainer: {
+    marginBottom: 20,
+  },
+  passwordContainer: {
+    marginBottom: 20,
+  },
+  usernameTitle: {
+    color: "#FFFFFF",
+    fontSize: 20,
+    fontWeight: "semibold",
+    textAlign: "left",
+  },
+  input: {
+    backgroundColor: "#2A2A37",
+    color: "#FFFFFF",
+    fontSize: 16,
+    padding: 16,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  button: {
+    backgroundColor: "#FF9001",
+    color: "#FFFFFF",
+    padding: 16,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "black",
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  redirectLink: {
+    color: "#CDCDE0",
+    fontSize: 16,
+    textAlign: "center",
+    marginTop: 10,
+  },
+  login: {
+    color: "#FF9001",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
